3f95b786f9756b6a653dcd64a5530411cdd892f4
Bryan Leboff <bleboff@appirio.com>
2019-01-28 16:00:39 -0500

    fix(menu): add reload/refresh to all versions not just osx

--- app/menu.js
+++ app/menu.js
@@ -245,12 +245,26 @@ export default class MenuBuilder {
               ]
             : [
                 {
+                  label: '&Reload',
+                  accelerator: 'Ctrl+R',
+                  click: () => {
+                    this.mainWindow.webContents.reload();
+                  }
+                },
+                {
                   label: 'Toggle &Full Screen',
                   accelerator: 'F11',
                   click: () => {
                     this.mainWindow.setFullScreen(
                       !this.mainWindow.isFullScreen()
                     );
+                  }
+                },
+                {
+                  label: 'Toggle &Developer Tools',
+                  accelerator: 'Alt+Ctrl+I',
+                  click: () => {
+                    this.mainWindow.toggleDevTools();
                   }
                 }
               ]
